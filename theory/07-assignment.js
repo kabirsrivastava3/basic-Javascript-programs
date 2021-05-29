c6896cec83e33619edcdb8f28b031fdd0aabb091
Kabir Srivastava <kabirsrivastava12@gmail.com>
2021-05-30 01:23:53 +0530

    [Add] question files

--- theory/07-assignment.js
+++ theory/07-assignment.js
@@ -6,46 +6,46 @@ var minusy = 10;
 minusy -= 5;
 console.log(minusy);
 
-var multiplyz= 10;
+var multiplyz = 10;
 multiplyz *= 5;
 console.log(multiplyz);
 
-var dividea= 10;
+var dividea = 10;
 dividea /= 5;
 console.log(dividea);
 
-var multib= 10;
+var multib = 10;
 multib *= 5;
 console.log(multib);
 
-var moduloc= 10;
+var moduloc = 10;
 modiuloc %= 5;
 console.log(moduloc);
 
-var shifte= 10;
+var shifte = 10;
 shifte <<= 5;
 console.log(shifte);
 
-var shiftf= 10;
+var shiftf = 10;
 shiftf >>= 5;
 console.log(shiftf);
 
-var shiftg= 10;
+var shiftg = 10;
 shiftg >>>= 5;
 console.log(shiftg);
 
-var andh= 10;
+var andh = 10;
 andh &= 5;
 console.log(andh);
 
-var mi= 10;
+var mi = 10;
 mi ^= 5;
 console.log(mi);
 
-var bridgej= 10;
+var bridgej = 10;
 bridgej |= 5;
 console.log(bridgej);
 
-var powerk= 10;
+var powerk = 10;
 powerk **= 5;
 console.log(powerk);
