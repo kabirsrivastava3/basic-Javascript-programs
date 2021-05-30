03d4284fea2ecb0e6fcbd4fbb3be91bdcb10f83f
Kabir Srivastava <kabirsrivastava12@gmail.com>
2021-05-30 12:42:56 +0530

    [Add] String functions combined in one program

--- strings/04-lengthofstring.js
+++
@@ -1,9 +0,0 @@
-var text = "ABCDEFGHIJKLMN";
-var stringlength = txt.length;
-
-if (stringlength > 10) {
-    console.log("Error greater than 10");
-}
-else {
-    console.log(text);
-}
---
+++ strings/all-string-functions.js
@@ -0,0 +1,38 @@
+var text1 = "ABCDCDP1244CDP$%#^&@&#&&()CDP";
+var text2 = "ADD THIS";
+var reverseText1="";
+var traverse = 0, vowel, uppercase, consecutive1, consecutive2, specialchar;
+const last3 = 3;
+for (let index = 0; index < text1.length - 1; index++) {
+    traverse = text1.charAt(index);
+
+    if (traverse == 65 && traverse == 90) {
+        uppercase++;
+        consecutive1 = traverse + 1;
+    }
+    if (traverse == 97 && traverse == 122) {
+        consecutive2 = traverse + 1;
+    }
+    if (traverse == 32 && traverse == 47
+        || traverse == 58 && traverse == 64
+        || traverse == 91 && traverse == 96
+        || traverse == 123 && traverse == 126){
+            specialchar++;
+        }
+        if (traverse=='A'||traverse=='E'||traverse=='I'||traverse=='O'||
+        traverse=='U'||traverse=='a'||traverse=='e'||traverse=='i'||traverse=='o'||
+        traverse=='u') {
+            vowel++;
+        }
+         if (traverse=='CDP' && traverse=='cdp') {
+             
+         }
+         reverseText1=traverse+reverseText1;
+
+}
+               
+               
+               console.log(text1.substring(text1.length-last3));
+               var text3= text1.concat("",text2);
+               console.log(text3);
+               console.log(reverseText1);
